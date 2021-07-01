75b68a98d88003558003e0a44523d9125f5ff10c
Ghost <vladimirvsevolodovi@yandex.ru>
2021-07-01 22:48:23 +0300

    git: add tests/checkArrIdentify

---
+++ tests/checkArrIdentify.js
@@ -0,0 +1,15 @@
+module.exports = (arr1, arr2) => {
+	
+	if(!Array.isArray(arr2)) return false;
+	
+	if(arr1.length != arr2.length) return false;
+	
+	for(let i = 0; i < arr1.length; ++i) {
+		
+		if(arr1[i] !== arr2[i]) return false;
+		
+	}
+	
+	return true;
+	
+};
