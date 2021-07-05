2ffba8a009a020031d81f1f8598a6ff5beeb3387
Ghost <vladimirvsevolodovi@yandex.ru>
2021-07-05 16:19:42 +0300

    git: add tests/results

---
+++ tests/results.js
@@ -0,0 +1,14 @@
+module.exports = [
+
+	"Операция успешно завершена.\r\n",
+	"Операция успешно завершена.\r\n",
+	"value",
+	true,
+	["value", 123, "value2", "value", "value2"],
+	false,
+	false,
+	"Операция успешно завершена.\r\n",
+	false,
+	false,
+
+];
